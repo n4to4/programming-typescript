c5ad6eb37dc8f3ca5d8cdbb35b624fd827ba1ecb
n4to4 <n4to4k@gmail.com>
2021-06-09 22:17:52 +0900

    snapshot

--- chapter02/src/index.ts
+++ chapter02/src/index.ts
@@ -1,1 +1,36 @@
-console.log('Hello TypeScript');
+let c: {
+  firstName: string
+  lastName: string
+} = {
+  firstName: 'john',
+  lastName: 'wick'
+}
+
+class Person {
+  constructor(
+    public firstName: string,
+    public lastName: string
+  ) { }
+}
+
+c = new Person('matt', 'smith')
+//console.log(c)
+
+//---
+
+let a: {
+  b: number
+  c?: string
+  [key: number]: boolean
+}
+a = { b: 1 }
+a = { b: 1, c: undefined }
+a = { b: 1, 10: true, 20: false }
+//a = { 10: true }
+//a = { b: 1, 33: 'red' }
+
+{
+  let as: readonly number[] = [1, 2, 3]
+  let bs: readonly number[] = as.concat(4)
+  console.log(bs)
+}
